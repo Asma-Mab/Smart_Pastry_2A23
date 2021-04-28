75fdb95b595324f703284acb5bbbf02e1f343204
FarahBK <farah.braiki@esprit.tn>
2021-04-28 01:53:46 +0100

    integration gestion employé congé

---
+++ connexion.cpp
@@ -0,0 +1,26 @@
+#include "connexion.h"
+#include"QDebug"
+#include"QSqlError"
+
+Connection::Connection()
+{
+
+}
+
+bool Connection::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("Source_Projet2A");//inserer le nom de la source de données ODBC
+db.setUserName("aziz");//inserer nom de l'utilisateur
+db.setPassword("esprit20");//inserer mot de passe de cet utilisateur
+
+if (db.open())
+test=true;
+
+
+qDebug()<<db.lastError();
+
+
+
+    return  test;
+}
